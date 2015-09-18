026d9be2d284fbb9ea0532b1118d036c860b0a6f
Duda Dornelles <dudassdornelles@gmail.com>
2015-09-17 19:08:09 -0700

    fixing tests

--- src/repository.rs
+++ src/repository.rs
@@ -13,7 +13,6 @@ fn should_add_many_values_with_threds_with_an_id() {
     }).collect();
 
     for h in handles { h.join().unwrap(); }
-
     
     let actual = repo.get("0".to_string());
     assert_eq!(actual, "abc");
@@ -25,12 +24,11 @@ fn should_work_with_a_complex_struct() {
 
     let handles: Vec<_> = vec![0,1,2].into_iter().map(|i| {
         let repo = repo.clone();
-        let t = Todo::new(i.to_string(), String::from("O gosh..."));
+        let t = Todo::new(i.to_string(), String::from("O gosh..."), false);
         return thread::spawn(move|| repo.add(i.to_string(), t));
     }).collect();
 
     for h in handles { h.join().unwrap(); }
-
     
     let actual = repo.get("0".to_string());
     assert_eq!(actual.title, String::from("O gosh..."));
@@ -43,7 +41,8 @@ fn should_return_all() {
     repo.add(0.to_string(), 10);
     repo.add(1.to_string(), 20);
 
-    let all: Vec<u32> = repo.all();
+    let mut all: Vec<u32> = repo.all();
+    all.sort();
 
     assert_eq!(all[0], 10);
     assert_eq!(all[1], 20);
